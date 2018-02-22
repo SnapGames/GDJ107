a7707ea2a39410854211ea6b30c4124969190856
Frédéric Delorme <frederic.delorme@snapgames.fr>
2018-02-22 01:27:03 +0100

    add some gfx mockup

---
+++ src/main/resources/res/maps/tiles-0001.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Sprite-0001" tilewidth="16" tileheight="16" tilecount="240" columns="20">
+ <image source="../images/Sprite-0001.png" trans="ff00ff" width="320" height="200"/>
+ <tile id="3">
+  <properties>
+   <property name="energy" type="int" value="20"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="weapon" type="int" value="10"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="weapon" type="int" value="100"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="weapon" type="int" value="50"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="life" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="mana" type="int" value="20"/>
+  </properties>
+ </tile>
+</tileset>
